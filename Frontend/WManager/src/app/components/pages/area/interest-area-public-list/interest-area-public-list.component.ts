35a8dff6d4a9e15cffc28e4a3821a621dea6f710
Umberto Aloisio <57840350+Ris8TR@users.noreply.github.com>
2025-01-06 15:06:57 +0100

    piccolo push

--- Frontend/WManager/src/app/components/pages/area/interest-area-public-list/interest-area-public-list.component.ts
+++ Frontend/WManager/src/app/components/pages/area/interest-area-public-list/interest-area-public-list.component.ts
@@ -7,20 +7,21 @@ import {CookieService} from "ngx-cookie-service";
 import {ToolbarComponent} from "../../../elements/toolbar/toolbar.component";
 import {ActivatedRoute, Router} from "@angular/router";
 import {InterestAreaDto} from "../../../../model/interestAreaDto";
-import {NgForOf} from "@angular/common";
+import {NgForOf, NgIf} from "@angular/common";
 import {InterestAreaDataService} from "../../../../service/InterestAreaDataService";
 
 @Component({
   selector: 'app-interest-area-public-list',
   standalone: true,
   imports: [
-    NgForOf
+    NgForOf,
+    NgIf
   ],
   templateUrl: './interest-area-public-list.component.html',
   styleUrl: './interest-area-public-list.component.css'
 })
 export class InterestAreaPublicListComponent implements OnInit {
-  protected interestAreaList!: InterestAreaDto[];
+  protected interestAreaList: InterestAreaDto[] = []; // Initialized as an empty array
 
   constructor(
     private interestAreaService: InterestAreaService,
@@ -29,9 +30,15 @@ export class InterestAreaPublicListComponent implements OnInit {
   ) {}
 
   ngOnInit(): void {
-    this.interestAreaService.getAllPublicInterestArea().subscribe((area) => {
-      this.interestAreaList = area;
-    });
+    this.interestAreaService.getAllPublicInterestArea().subscribe(
+      (area) => {
+        this.interestAreaList = area;
+      },
+      (error) => {
+        console.error("Error fetching public areas:", error);
+        this.interestAreaList = []; // Ensure list is empty on error
+      }
+    );
   }
 
   redirect(area: InterestAreaDto): void {
